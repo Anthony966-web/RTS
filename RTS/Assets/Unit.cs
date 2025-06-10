e78147d434a75f56fd966a89edca9208d440a5e8
MUKILTEO\1722415 <1722415>
2025-06-10 10:11:21 -0700

    Added More

---
+++ RTS/Assets/Unit.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    private void Start()
+    {
+        UnitSelectionManager.Instance.allUnitsList.Add(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        UnitSelectionManager.Instance.allUnitsList.Remove(gameObject);
+    }
+}
